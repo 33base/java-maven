022977e531cd344dabe7435832b5e4b95514aa4b
33base <setikhonenko@gmail.com>
2023-03-13 17:47:58 +0200

    new version

--- script.groovy
+++ script.groovy
@@ -2,16 +2,15 @@ def buildApp() {
     echo 'building the aplication'
 }
 
-return this 
 
 def testApp() {
     echo 'testing the aplication'
 }
 
-return this 
 
 def deployApp() {
     echo 'Hello learning jenkins is cool!!'
     echo "deploying version ${params.VERSION}"
+}
 
-return this 
+return this
