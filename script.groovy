2c2d048923c1e8ad7cf62ddefa0cc644a68874c5
33base <setikhonenko@gmail.com>
2023-03-14 12:31:03 +0200

    new version

--- script.groovy
+++ script.groovy
@@ -1,7 +1,15 @@
-def buildApp() {
+def buildJar() {
     echo 'building the aplication'
+    sh 'mvn package'
 }
 
+def buildImage() {
+    echo 'building the docker image'
+    withCredentials ([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+            sh 'docker build -t 33base/myrepo:jma-2.0 .'
+            sh "echo $PASS | docker login -u $USER --password-stdin"
+            sh 'docker push 33base/myrepo:jma-2.0'
+}
 
 def testApp() {
     echo 'testing the aplication'
@@ -10,7 +18,6 @@ def testApp() {
 
 def deployApp() {
     echo 'Hello learning jenkins is cool!!'
-    echo "deploying version ${params.VERSION}"
 }
 
 return this
